5cdd6ac04713d6d6c77f2743fa592daea26bbe31
httpcyberdata <borgeringstad@Brge-Ringstad-PC.local>
2022-12-03 17:52:05 -0500

    cleaning Post.tsx

--- frontend/src/features/posts/Posts.tsx
+++ frontend/src/features/posts/Posts.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useAppSelector } from '../../app/hooks';
+import { fetchPostsAsync, selectPosts } from './postsSlice';
 
 function Posts() {
+	const posts = useAppSelector(selectPosts);
 	return <div>
 		<h1>Posts </h1>
 	</div>
