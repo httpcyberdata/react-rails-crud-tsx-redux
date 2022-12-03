97af4a076bc884840d5c893a8f38ca216c77d672
httpcyberdata <borgeringstad@Brge-Ringstad-PC.local>
2022-12-03 17:36:47 -0500

    initial

---
+++ frontend/src/features/posts/postAPI.ts
@@ -0,0 +1,6 @@
+import { PostsState } from './postSlice';
+
+const API_URL = "http://localhost:3000";
+
+export async function fetchPosts() {
+	return {}
---
+++ frontend/src/features/posts/postSlice.tsx
@@ -0,0 +1,77 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import produce from 'immer';
+import { RootState } from '../../app/store';
+
+
+export enum Statuses {
+  Initial = 'Not fetched',
+  Loading = "Loading...",
+  UpToDate = "Up to Date",
+  Deleted = "Deleted",
+  Error = "Error"
+}
+
+export interface PostState {
+  id?: number;
+  title?: string;
+  body?: string;
+  created_at?: any;
+  updated_at?: any;
+}
+
+export interface PostsState {
+  posts: PostState[];
+  status: string;
+}
+
+
+const initialState: PostsState = {
+  posts: [
+    {
+    id: 0,
+    title: "",
+    body: "I am here.",
+    created_at: "",
+    updated_at: "",
+    }
+  ],
+  status: Status.Initial
+}
+
+export const fetchPostsAsync = createAsyncThunk(
+    'posts/fetchPosts',
+    async () => {
+      const response = await fetchPosts();
+      return response;
+    }
+  )
+
+export const postSlice = createSlice({
+  name: 'posts',
+  initialState, 
+  reducers: {}, 
+  extraReducers: (builder) => {
+    builder
+        .addCase(fetchPostsAsync.pending, (state) => {
+          return produce(state, (draftState) => {
+            draftState.status = Statuses.Loading;
+          })
+        })        
+        .addCase(fetchPostsAsync.fulfilled (state) => {
+          return produce(state, (draftState) => {
+              draftState.status = Statuses.UpToDate;
+          })
+        })        
+         .addCase(fetchPostsAsync.error, (state) => {
+          return produce(state, (draftState) => {
+            draftState.status = Statuses.Error;
+          })
+        })
+    }
+})
+
+export const {  } = postSlice.actions;
+export const selectPosts = ( state: RootState ) => state.posts.posts;
+export const selectStatus = (state: RootState ) => state.posts.status;
+
+export default postSlice.reducer;
